d07d29e4e91e7cc02170c69a732ea6b7e0e1725c
Rahma A <64001826+Kryptos1015@users.noreply.github.com>
2021-07-06 11:52:49 +0400

    Update bump-bot.py

--- bump-bot.py
+++ bump-bot.py
@@ -28,9 +28,8 @@ async def on_ready():
 
 @client.event
 async def on_guild_join(guild):
-    await helpsheet('x')
     await get_channel('x')
-    await bump()
+    await helpsheet('x')
 
 @client.command()
 async def bumpbot(ctx):
